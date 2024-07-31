9d7724fa5a5436abb27e51e60651894313f15a84
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-08-01 00:04:26 +0530

    Added Music player UI component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Sidebar from "./components/Sidebar";
+import Player from "./components/Player";
 const App = () => {
-  return <div className="h-screen bg-black">
-    <div className="h-[90%] flex">
-    <Sidebar/>
+  return (
+    <div className="h-screen bg-black">
+      <div className="h-[90%] flex">
+        <Sidebar />
+      </div>
+      <Player />
     </div>
-  </div>;
+  );
 };
 export default App;
