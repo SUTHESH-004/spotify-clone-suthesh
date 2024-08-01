5d71fe156629fe8fd64af3a9e20288ade658bfc0
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-08-01 20:20:36 +0530

    added navbar with various buttons

---
+++ src/components/DisplayHome.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar1 from "./Navbar1";
+const DisplayHome = () => {
+  return (
+    <>
+      <Navbar1 />
+      <div className="my-5 font-bold text-2xl">
+        
+      </div>
+    </>
+  );
+};
+
+export default DisplayHome;
