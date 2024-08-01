4970ac93c58e44981aa0ab7544649b62f5eb537b
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-08-01 23:11:47 +0530

    added songsitems & albumitems

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,11 +1,43 @@
 import React from "react";
 import Navbar1 from "./Navbar1";
+// import Albumitems from "./Albumitems";
+import { albumsData } from "../assets/assets";
+import Albumitems from "./Albumitems";
+import { songsData } from "../assets/assets";
+import Songitems from "./Songitems";
 const DisplayHome = () => {
   return (
     <>
       <Navbar1 />
-      <div className="my-5 font-bold text-2xl">
-        
+
+      <div></div>
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((item, index) => (
+            <Albumitems
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Today's biggest hits</h1>
+        <div className="flex overflow-auto">
+          {songsData.map((item, index) => (
+            <Songitems
+              key={index}
+              name={item.name}
+              desc="item.desc"
+              id={item.desc}
+              image={item.image}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
