f90ed22acc61c232650d3eccb33fdd6c44320750
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-08-02 18:09:50 +0530

    added dynamic background- color using use effect

---
+++ src/components/DisplayAlbum.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import Navbar1 from "./Navbar1";
+import { useParams } from "react-router-dom";
+import { albumsData, assets, songsData } from "../assets/assets";
+
+const DisplayAlbum = () => {
+  const { id } = useParams();
+  //   console.log(id);
+  // for checking the id
+  const albumData = albumsData[id];
+  console.log(albumData);
+  return (
+    <>
+      <Navbar1 />
+      <div className="mt-10 flex gap-8 flex-col md:flex-row md:items-end">
+        <img className="w-48 rounded" src={albumData.image} alt="" />
+        <div flex flex-col>
+          <p>PlayList</p>
+          <h1 className="text-5xl font-bold mb-4 md-4 md:text-xl">
+            {albumData.name}
+          </h1>
+          <h4>{albumData.desc}</h4>
+          <p className="mt-1">
+            <img
+              className=" inline-block w-5"
+              src={assets.spotify_logo}
+              alt=""
+            />
+            <b>Spotify</b>
+            1,323,154 likes
+            <b>50 songs,</b>
+            about 2 hr 30 min
+          </p>
+        </div>
+      </div>
+      <div className="grid grid-cols-3 sm:grid-cols-4 mt-10 mb-4 pl-2 text-[#a7a7a7]">
+        <p>
+          <b className="mr-4">#</b>Title
+        </p>
+        <p>Labum</p>
+        <p className="hidden sm:block">Date Added</p>
+        <img className="m-auto w-4" src={assets.clock_icon} alt="" />
+      </div>
+      <hr />
+      {songsData.map((item, index) => (
+        <div
+          key={index}
+          className="grid grid=cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer"
+        >
+          <p className="text-white">
+            <b className="mr-4 text-[#a7a7a7]">{index + 1}</b>
+            <img className="inline w-10 mr-5" src="" alt="" />
+            {item.name}
+          </p>
+          <p className="text-[15px]">{albumData.name}</p>
+          <p className="text-[15px] hidden sm:block">5 days ago</p>
+          <p className="text-[15px] text-center">{item.duration}</p>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default DisplayAlbum;
--- src/components/Navbar1.jsx
+++ src/components/Navbar1.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 const Navbar1 = () => {
+  const navigate = useNavigate()
   return (
     <>
       <div className="w-full flex justify-between items-center font-semibold">
         <div className="flex items-center gap-2">
-          <img
+          <img onClick={()=>navigate(-1)}
             className=" w-8 bg-black p-2 rounded-2x1 cursor-pointer"
             src={assets.arrow_left}
             alt=""
           />
-          <img
+          <img onClick={()=>navigate(1)}
             className=" w-8 bg-black p-2 rounded-2x1 cursor-pointer"
             src={assets.arrow_right}
             alt=""
