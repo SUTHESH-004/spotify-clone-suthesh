de0eff2d09d4bc3a1d268390ab26ad18fcd73d3f
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-08-23 20:05:21 +0530

    completed the front end of the application

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,6 +6,7 @@ const Sidebar = () => {
   return (
     <div className="w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex">
       <div className="bg-[#121212] h-[15%] rounded flex flex-col justify-around">
+        
         <div
           onClick={() => navigate("/")}
           className="flex items-center gap-3 pl-8 cursor-pointer"
